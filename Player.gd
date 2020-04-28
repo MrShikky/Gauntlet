7adacf703e1aac0348b8cefc78c7cf398eed1491
jukster11 <shikhar.s.pachchigar@gmail.com>
2020-04-28 00:42:50 -0400

    IDEK MAN IM HIGH AS SHIT

--- Player.gd
+++ Player.gd
@@ -1,32 +1,38 @@
 extends KinematicBody2D
 
 # Declare member variables here. Examples:
+var Player = Vector2()
+var Boss = Vector2()
+export (int) var linear_velocity = 600
+export (float) var angular_velocity
+export (float) var radius
+export var velocity_vector = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	Boss = get_viewport_rect().size
 
-export (int) var speed = 600
-export (float) var radius = 200
-export (float) var rotation_speed = speed / radius
-
-var velocity = Vector2()
 var rotation_dir = 0
 
 func get_input():
+	Player = get_global_position()
+	radius = sqrt(pow(Boss.y - Player.y,2)+pow(Boss.x - Player.x,2))
+	angular_velocity = linear_velocity / radius
+	
 	rotation_dir = 0
-	velocity = Vector2()
+	velocity_vector = Vector2()
 	if Input.is_action_pressed("ui_left"):
-		velocity = Vector2(-speed, 0).rotated(rotation)
+		velocity_vector = Vector2(angular_velocity, 0).rotated(rotation)
+		print(velocity_vector)
 		rotation_dir += 1
 	if Input.is_action_pressed("ui_right"):
-		velocity = Vector2(speed, 0).rotated(rotation)
+		velocity_vector = Vector2(-angular_velocity, 0).rotated(rotation)
 		rotation_dir -= 1
 
-func _process(delta):
+func _physics_process(delta):
 	get_input()
-	rotation += rotation_dir * rotation_speed * delta
-	velocity += move_and_slide(velocity)
+	rotation += rotation_dir * angular_velocity * delta
+	velocity_vector += move_and_slide(velocity_vector)
 	
 
 
